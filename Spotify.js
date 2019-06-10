109f85c639de66b98f4ac74370821520394d3234
Michael <otraymusic@gmail.com>
2019-06-10 14:03:24 +0200

    Add files via upload

---
+++ Spotify.js
@@ -0,0 +1,85 @@
+//import clientID from "./clientID.js"
+
+const clientID = "08a1d1ac7d8b498dac7a958bf68bb083"
+const redirectURI = "https://otray_jammming_deploy.surge.sh/" //window.location.href //"http://localhost:3000/"
+let accessToken = ""
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken.length) return accessToken
+
+        let urlAccessToken = undefined
+        let urlTokenExpiresIn = undefined
+
+        const url = window.location.href
+        if (url.match(/access_token=([^&]*)/)) {
+            urlAccessToken = url.match(/access_token=([^&]*)/)
+            urlTokenExpiresIn = url.match(/expires_in=([^&]*)/)
+        }
+
+        if (urlAccessToken && urlTokenExpiresIn) {
+            accessToken = urlAccessToken[1]
+            const expiresIn = urlTokenExpiresIn[1]
+
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken
+        }
+        else {
+            window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+        }
+    },
+
+    async search(term) {
+        accessToken = this.getAccessToken()
+
+        return await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+                headers: { Authorization: `Bearer ${accessToken}` }
+            })
+            .then(response => response.json()).then(jsonResponse => {
+                if (!jsonResponse.tracks) return []
+                return jsonResponse.tracks.items.map(track => {
+                    return {
+                        id: track.id,
+                        name: track.name,
+                        artist: track.artists[0].name,
+                        album: track.album.name,
+                        uri: track.uri
+                    }
+                })
+            })
+    },
+
+    savePlaylist(playlistName, trackURIs) {
+        if (!playlistName.length && !trackURIs.length) return
+
+        accessToken = this.getAccessToken()
+        const headers = { Authorization: `Bearer ${accessToken}` }
+        let userID
+
+        fetch(`https://api.spotify.com/v1/me`, { headers: headers })
+            .then(response => response.json()).then(jsonResponse => {
+                userID = jsonResponse.id
+
+                // create playlist w/ tracks
+                return fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+                    headers: headers,
+                    method: "POST",
+                    body: JSON.stringify({ name: playlistName })
+
+                }).then(response => response.json()).then(jsonResponse => {
+                    const playlistID = jsonResponse.id
+
+                    // save tracks to playlist
+                    return fetch(`https://api.spotify.com/v1/users/${userID}/playlists/${playlistID}/tracks`, {
+                        headers: headers,
+                        method: "POST",
+                        body: JSON.stringify({ uris: trackURIs })
+                    })
+                })
+            })
+    }
+}
+
+
+export default Spotify
---
+++ precache-manifest.4cb129e633036a0c8ba88c1f6f100117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f2021aee9cc675e763c333ee252eb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65e3319c733a702dfac",
+    "url": "/static/css/main.937dcffa.chunk.css"
+  },
+  {
+    "revision": "7f215f5c1fa34f2f8617",
+    "url": "/static/js/2.443224c8.chunk.js"
+  },
+  {
+    "revision": "b65e3319c733a702dfac",
+    "url": "/static/js/main.88fbbc4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
